1aee971d9ab7df645e7cec8087e16be540afdd47
jisenen <506700741@qq.com>
2021-03-17 22:14:40 +0800

    增加一些逻辑代码

--- app/src/main/java/com/mylearn/jeyepetizer/ui/home/HomePageFragment.kt
+++ app/src/main/java/com/mylearn/jeyepetizer/ui/home/HomePageFragment.kt
@@ -8,12 +8,16 @@ import android.view.ViewGroup
 import com.flyco.tablayout.listener.CustomTabEntity
 import com.mylearn.jeyepetizer.R
 import com.mylearn.jeyepetizer.common.ui.BaseViewPagerFragment
+import com.mylearn.jeyepetizer.event.MessageEvent
+import com.mylearn.jeyepetizer.event.RefreshEvent
+import com.mylearn.jeyepetizer.event.SwitchPagesEvent
 import com.mylearn.jeyepetizer.logic.entity.TabEntity
 import com.mylearn.jeyepetizer.ui.home.commend.CommendFragment
 import com.mylearn.jeyepetizer.ui.home.daily.DailyFragment
 import com.mylearn.jeyepetizer.ui.home.discovery.DiscoveryFragment
 import com.mylearn.jeyepetizer.util.GlobalUtil
 import kotlinx.android.synthetic.main.layout_main_page_title_bar.*
+import org.greenrobot.eventbus.EventBus
 
 /**
  * @Author: JSS
@@ -47,7 +51,26 @@ class HomePageFragment : BaseViewPagerFragment() {
         viewPager?.currentItem = 1
     }
 
-
+    override fun onMessageEvent(messageEvent: MessageEvent) {
+        super.onMessageEvent(messageEvent)
+        if (messageEvent is RefreshEvent && this::class.java == messageEvent.activityClass) {
+            when (viewPager?.currentItem) {
+                0 -> EventBus.getDefault().post(RefreshEvent(DiscoveryFragment::class.java))
+                1 -> EventBus.getDefault().post(RefreshEvent(CommendFragment::class.java))
+                2 -> EventBus.getDefault().post(RefreshEvent(DailyFragment::class.java))
+                else -> {
+                }
+            }
+        } else if (messageEvent is SwitchPagesEvent) {
+            when (messageEvent.activityClass) {
+                DiscoveryFragment::class.java -> viewPager?.currentItem = 0
+                CommendFragment::class.java -> viewPager?.currentItem = 1
+                DailyFragment::class.java -> viewPager?.currentItem = 2
+                else -> {
+                }
+            }
+        }
+    }
 
     companion object {
         fun newInstance() = HomePageFragment()
