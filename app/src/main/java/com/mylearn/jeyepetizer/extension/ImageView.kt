a9a824588e925a599401d46207fa6419070afae0
Jisenen <506700741@qq.com>
2021-04-08 09:56:48 +0800

    1.上传一下

--- app/src/main/java/com/mylearn/jeyepetizer/extension/ImageView.kt
+++ app/src/main/java/com/mylearn/jeyepetizer/extension/ImageView.kt
@@ -19,7 +19,8 @@ package com.mylearn.jeyepetizer.extension
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.eyepetizer.android.R
+import com.mylearn.jeyepetizer.R
+
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
 /**
@@ -33,7 +34,8 @@ fun ImageView.load(url: String, round: Float = 0f, cornerType: RoundedCornersTra
     if (round == 0f) {
         Glide.with(this.context).load(url).into(this)
     } else {
-        val option = RequestOptions.bitmapTransform(RoundedCornersTransformation(dp2px(round), 0, cornerType)).placeholder(R.drawable.shape_album_loading_bg)
+        val option = RequestOptions.bitmapTransform(RoundedCornersTransformation(dp2px(round), 0, cornerType)).placeholder(
+            R.drawable.shape_album_loading_bg)
         Glide.with(this.context).load(url).apply(option).into(this)
     }
 }
